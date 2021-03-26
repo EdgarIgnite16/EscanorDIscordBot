7b4408fdb6a9ad4a4fe97014a9dd305d4d28cfcb
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-03-27 00:39:19 +0700

    realese

---
+++ commands/8ball.js
@@ -0,0 +1,49 @@
+const { stripIndent } = require("common-tags");
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  name: "8ball",
+  description: "There is a big chance I insult you!",
+  category: "fun",
+  async run (bot, message, args) {
+    let question = message.content.slice(8);
+    message.delete()
+    if (!question)
+      return message.channel.send(`You did not specify your question!`).then((sent) => {
+        setTimeout(() => {
+            sent.delete();
+        }, 5000);
+    });
+    else {
+      let responses = [
+        "Có",
+        "Không",
+        "Đoán Xem",
+        "Chắc chắn không ! ",
+        "Chắn chắn có ! ",
+        "Có nhưng không ",
+        "100%",
+        "99%",
+        "50-50",
+        "49-51",
+        "Tao Chịu",
+        "Đang Search..........",
+        "Thử Lại đi câu này t nghe không rõ...",
+      ];
+      let response =
+        responses[Math.floor(Math.random() * responses.length - 1)];
+      let Embed = new MessageEmbed()
+        .setTitle(`8Ball Bủh Bủh lmeo !`)
+        .setThumbnail("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLtNCR2P2prQ_rTY1f0eCFA87jXM0cA6imusO8bPzcx_8eFO0GqQEKAcrQOBLb9T0Y2IU&usqp=CAU")
+        .addField("Best Question of Day !🎉", stripIndent`
+        \n
+        **❓ Your Question:**\n${question}
+        
+        **📫 My reply: **\n${response}
+        `)
+        .setTimestamp()
+        .setFooter("User Call Bot: " + message.author.username)
+        .setColor(`RANDOM`);
+      message.channel.send(Embed);
+    }
+  },
+};
---
+++ commands/user-info.js
@@ -0,0 +1,54 @@
+const Discord = require("discord.js")
+module.exports = {
+    name: "user-info",
+    desciption: "get infos of user",
+
+
+  async  run(client, message, args) {
+        message.delete();
+        //check if more than 1 user is mentioned
+        if (args.length > 1) return message.channel.send('Only mention one user!').then((sent) => {
+            setTimeout(() => {
+                sent.delete();
+            }, 5000);
+        });
+
+        //check if there is no arguments
+        if (!args[0]) return message.channel.send('Mention someone!').then((sent) => {
+            setTimeout(() => {
+                sent.delete();
+            }, 5000);
+        });
+        
+        //check if there is 1 argument
+        if (args[0]) {
+            //get the first user mentioned
+            let member = message.mentions.members.first()
+           
+            //if the member exists create an embed with info about that user and send it to the channel
+            if (member) {
+                let embed = new Discord.MessageEmbed()
+                    .setColor("RANDOM")
+                    .setTitle("User Info")
+                    .setThumbnail(member.user.displayAvatarURL())
+                    .addField("**Username:**", `${member.user.username}`, true)
+                    .addField("**Discriminator:**", `#${member.user.discriminator}`, true)
+                    .addField("**ID:**", `${member.user.id}`, true)
+                    .addField("**Status:**", `${member.user.presence.status}`, true)
+                    .addField("**Joined On:**", `${member.joinedAt.toLocaleString()}`, true)
+                    .addField("**Created On:**", `${member.user.createdAt.toLocaleString()}`, true)
+                    .setDescription(`${member.roles.cache.map(role => role.toString()).join(' ')}`)
+                    .setFooter("User Call Bot: " + message.author.username)
+                    .setTimestamp()
+
+                message.channel.send(embed);
+            } else {
+                message.channel.send(`Could not find that member`).then((sent) => {
+                    setTimeout(() => {
+                        sent.delete();
+                    }, 5000);
+                });//send a message to the channel if the user doesn't exist
+            }
+        }
+    }
+}
