f9030ad79fb485100113d925e4ef3a69022b121c
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-03-27 16:13:01 +0700

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+const { stripIndents } = require("common-tags")
+module.exports = {
+    name: "help",
+    desciption: "help commands",
+
+    async run (client, message) {
+        message.delete();
+        let embed = new Discord.MessageEmbed()
+            .setThumbnail(message.guild.iconURL())
+            .setTitle("Help Commands !")
+            .setDescription("**Nhập Prefix ``a!``+``<Lệnh>`` để sử dụng **")
+            .setColor("#8de815")
+            .addField("🔐 Moderator:"," ``svinfo`` | ``user-info`` | ``ban`` | ``kich``")
+            .addField("🧾 Feature:"," ``avatar`` | ``say`` | ``dms`` | ``cfs`` | ``weather`` | ``clear`` | ``ping``")
+            .addField("🎆 Fun:"," ``8ball`` | ``iq`` | ``meme`` | ``slap`` | ``kiss`` | ``hug`` | ``baka``")
+            .addField("⛔ NSFW:"," ``ass`` | ``anal`` | ``blowjob`` | ``boobs`` | ``cum`` | ``ero`` | ``erofeet`` | ``foxgirl``")
+            .addField("🐾Animal:"," ``cat`` | ``dog``")
+            .setFooter("User Call Bot: " + message.author.username)
+            .setTimestamp()
+        message.channel.send(embed)
+
+    }
+}
