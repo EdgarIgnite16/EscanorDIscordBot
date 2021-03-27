4de2771f752101c31840afb0ff9e202a14fd3466
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-03-27 14:05:24 +0700

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,38 @@
+const randomPuppy = require('random-puppy');
+const Discord = module.require("discord.js");
+
+module.exports = { 
+    name: "meme",
+    description: "Sends a random meme",
+    async run( client , message , args){
+        let reddit = [
+            "meme",
+            "animemes",
+            "MemesOfAnime",
+            "animememes",
+            "AnimeFunny",
+            "dankmemes",
+            "dankmeme",
+            "wholesomememes",
+            "MemeEconomy",
+            "techsupportanimals",
+            "meirl",
+            "me_irl",
+            "2meirl4meirl",
+            "AdviceAnimals"
+        ]
+    
+        let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
+    
+        message.channel.startTyping();
+    
+        randomPuppy(subreddit).then(async url => {
+                await message.channel.send({
+                    files: [{
+                        attachment: url,
+                        name: 'meme.png'
+                    }]
+                }).then(() => message.channel.stopTyping());
+        })
+    }
+}
