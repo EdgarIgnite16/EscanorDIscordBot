af94d1bc428416ee6aaf2f3b477af8a9756ed052
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-03-27 18:53:43 +0700

    Update user-info.js

--- commands/user-info.js
+++ commands/user-info.js
@@ -37,7 +37,7 @@ module.exports = {
                     .addField("**ID:**", `${member.user.id}`, true)
                     .addField("**Joined On:**", `${member.joinedAt.toLocaleString()}`, true)
                     .addField("**Created On:**", `${member.user.createdAt.toLocaleString()}`, true)
-                    .addField("**Role: **",`${member.roles.cache.map(role => role.toString()).join(' ')}`)
+                    .addField("**Permission: **",`${member.roles.cache.map(role => role.toString()).join(' ')}`)
                     .setFooter("User Call Bot: " + message.author.username)
                     .setTimestamp()
 
