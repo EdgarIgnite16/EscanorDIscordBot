b174a049b0b4e9d3587baaf05731aea09df2a9d5
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-03-29 01:13:48 +0700

    Update AIchat.js

--- commands/AIchat.js
+++ commands/AIchat.js
@@ -10,7 +10,7 @@ module.exports = {
         
         const res = await axios.get(`http://api.brainshop.ai/get?bid=155429&key=mxAU5RVksNAqBKAQ&uid=1&msg=${encodeURIComponent(args.join(' '))}`);
         
-        let question = message.content.slice(7);
+        let question = message.content.slice(9);
         message.delete();
         let embed = new Discord.MessageEmbed()
             .setThumbnail(`https://i.pinimg.com/736x/81/a1/68/81a168b12a2d827c03dbf2f5c22c4761.jpg`)
