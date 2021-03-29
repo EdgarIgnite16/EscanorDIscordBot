286fc4b720449204bc31d6ff01e757e1f6d9e4e6
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-03-30 00:16:12 +0700

    Update baka.js

--- commands/baka.js
+++ commands/baka.js
@@ -7,18 +7,21 @@ module.exports = {
     async run( client , message , args ){
         message.delete();
         const user = message.mentions.users.first();
-        superagent.get('https://nekos.life/api/v2/img/baka')
-        .end((err, response) => {
-      const embed = new Discord.MessageEmbed()
-      .setTitle("BAKA!!! ")
-      .setDescription("ばかやろう "+ user.toString())
-      .setImage(response.body.url)
-      .setColor(`RANDOM`)
-      .setFooter(`idiot!` + "- User Call Bot: " + message.author.username)
-      .setURL(response.body.url);
-  message.channel.send(embed);
-    })
-
-
+        if(!user) return;
+        else{
+          superagent.get('https://nekos.life/api/v2/img/baka')
+          .end((err, response) => {
+        const embed = new Discord.MessageEmbed()
+        .setTitle("BAKA!!!")
+        .setDescription("ばかやろう "+ user.username)
+        .setImage(response.body.url)
+        .setColor(`RANDOM`)
+        .setFooter(`idiot!` + "- User Call Bot: " + message.author.username)
+        .setURL(response.body.url);
+        message.channel.send(embed);
+      })
+  
+  
+        }
     }
 } 
