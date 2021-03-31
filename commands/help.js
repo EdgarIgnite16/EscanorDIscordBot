b9379b1128655f6e2508a2b289f299ef81235751
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-03-31 16:59:35 +0700

    re

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
             .setDescription("**Nhập Prefix `e!` + `<Lệnh>` để sử dụng **")
             .setColor("#8de815")
             .addField("🔐 Moderator:"," `sv-info` `user-info` `ban` `kick` `poll` ")
-            .addField("🧾 Feature:"," `chat` `avatar` `search` `cfs` `weather` `clear` `ping` `dms` `say` `emoji-list ` ")
+            .addField("🧾 Feature:"," `chat` `avatar` `search` `cfs` `weather` `clear` `ping` `dms` `say`  `emoji` `emoji-list ` ")
             .addField("🎆 Fun:"," `8ball` `iq` `meme` `slap` `kiss` `hug` `baka` `cat` `dog` ")
             .addField("⛔ NSFW (Not Recommend):"," `ass` `anal` `blowjob` `boobs` `cum` `ero` `erofeet` `foxgirl` ")
             .setFooter("User Call Bot: " + message.author.username)
