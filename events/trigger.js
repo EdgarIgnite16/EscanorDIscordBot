b5d90921196b707e29cafdc55ff645545e259595
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-04-21 00:26:59 +0700

    re

--- events/trigger.js
+++ events/trigger.js
@@ -4,5 +4,6 @@ module.exports = {
         if(message.content.includes("ed") || message.content.includes("Ed")){
             return message.channel.send("Vua ăn Hại , Coder Hạng Bét , Lươn Chúa !");
         }
+        
     }
 }
