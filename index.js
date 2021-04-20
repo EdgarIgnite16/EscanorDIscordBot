87f1137d8f32d9aaba0e68a1fd7acb8ea954950d
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-04-21 00:29:14 +0700

    t

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ for (const file of eventFiles) {
   if (event.once) {
       client.once(event.name, (...args) => event.execute(...args, client));
   } else {
-
     client.on(event.name, (...args) => event.execute(...args, client));
   }
 }
