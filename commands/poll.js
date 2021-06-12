c48aa2e0be781b289637bc4a46f77f50a3a4cdd0
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-06-12 14:17:26 +0700

    re

--- commands/poll.js
+++ commands/poll.js
@@ -4,26 +4,38 @@ module.exports = {
     name: "poll",
     description: "Create a poll",
     async run(client , message ,args){
-        const member = await message.guild.member(message.author);
-        const pollmessage = await args.join(" ");
-        message.delete()
         if (message.member.hasPermission("ADMINISTRATOR")){
-            if (pollmessage.length <= 0) return message.channel.send({embed: {
-                color: 16734039,
-                description: "You must provide a text to ask a question!"
-            }})
-             const embed = new Discord.MessageEmbed()
-                .setTitle(":ballot_box: " +`${member.nickname}`+ " đã mở một cuộc thăm dò !",)
-                .setColor("RANDOM")
-                .setDescription(pollmessage)
-                .setFooter("Lưu ý : React bên đưới để phản hồi  • Vote created by " + `${member.nickname}`,)
-                .setTimestamp()
-                const pollTopic = await message.channel.send({embed})
-                    await pollTopic.react(`✅`);
-                    await pollTopic.react(`👌`);
-                    await pollTopic.react(`😶`);
-                    await pollTopic.react(`🤔`);
-                    await pollTopic.react(`❌`);
+            const member = await message.guild.member(message.author);
+            let textChannel = message.mentions.channels.first();
+            if(textChannel){
+                message.channel.send("Gửi Tin Nhắn Thành Công !").then((message) =>{
+                    setTimeout(() =>{
+                        message.delete();
+                    }, 2000);
+                   });
+            } 
+            else{
+                message.channel.send("Vui Lòng Nhập Kênh Cần Gửi !").then((message) => {
+                    setTimeout(() => {
+                        message.delete();
+                    }, 5000);
+                });
+            }
+            const embed = new Discord.MessageEmbed();
+            if(textChannel){
+                 embed
+                    .setColor("RANDOM")
+                    .setTitle(":ballot_box: " +`${member.nickname}`+ " đã mở một cuộc thăm dò !",)
+                    .setDescription(args.slice(1).join(" "))
+                    .setFooter("Lưu ý : React bên đưới để phản hồi  • Vote created by " + `${member.nickname}`,)
+                    .setTimestamp()
+                    const pollTopic = await textChannel.send({embed})
+                        await pollTopic.react(`✅`);
+                        await pollTopic.react(`👌`);
+                        await pollTopic.react(`😶`);
+                        await pollTopic.react(`🤔`);
+                        await pollTopic.react(`❌`);
+            }
         }else{
             message.channel.send("you dont have administrator role to to this")
         }
