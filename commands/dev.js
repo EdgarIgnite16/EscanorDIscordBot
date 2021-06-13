fbf0a901566e04bf52e52108fcf83e5490e25b3d
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-06-13 13:51:45 +0700

    Update dev.js

--- commands/dev.js
+++ commands/dev.js
@@ -13,8 +13,8 @@ module.exports ={
             **Tên Thật: ** Trần Nguyên Lộc
             **Sở thích: ** Thích làm loser :>
             **Github: ** https://github.com/EdgarIgnite16
-            **Facebook: ** https://www.facebook.com/edgawar.me.me/
             **Discord Server: ** https://discord.gg/47XNaf8Hep
+            **Facebook: ** https://www.facebook.com/edgawar.me.me/
             `)
             .setThumbnail("https://scontent.fhan14-2.fna.fbcdn.net/v/t1.6435-9/151065826_831597224350719_4345458488072486548_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=tBmQps1UAp0AX_S6pSv&_nc_ht=scontent.fhan14-2.fna&oh=e46ab43907e3b3ae6b07f90efeb64c1a&oe=60CE97FB")
         message.channel.send(embed)
