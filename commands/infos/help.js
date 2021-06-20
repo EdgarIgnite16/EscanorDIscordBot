e78a12ba98c480a64c8598cfe3c2ef56bcb60183
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-06-21 02:37:51 +0700

    qưe

--- commands/infos/help.js
+++ commands/infos/help.js
@@ -1,48 +1,26 @@
+const Discord = require('discord.js')
+
 module.exports = {
+    
     name: 'help',
-    aliases: ['h'],
-    category: 'Core',
-    utilisation: '{prefix}help <command name>',
+    aliases: [],
+    category: 'moderator',
+    utilisation: '{prefix}help',
 
-    execute(client, message, args) {
-        if (!args[0]) {
-            const infos = message.client.commands.filter(x => x.category == 'Infos').map((x) => '`' + x.name + '`').join(', ');
-            const music = message.client.commands.filter(x => x.category == 'Music').map((x) => '`' + x.name + '`').join(', ');
+    execute(client, message) {
+        let embed = new Discord.MessageEmbed()
 
-            message.channel.send({
-                embed: {
-                    color: 'ORANGE',
-                    author: { name: 'Help pannel' },
-                    footer: { text: 'This bot uses a Github project made by Zerio (ZerioDev/Music-bot)' },
-                    fields: [
-                        { name: 'Bot', value: infos },
-                        { name: 'Music', value: music },
-                        { name: 'Filters', value: client.filters.map((x) => '`' + x + '`').join(', ') },
-                    ],
-                    timestamp: new Date(),
-                    description: `To use filters, ${client.config.discord.prefix}filter (the filter). Example : ${client.config.discord.prefix}filter 8D.`,
-                },
-            });
-        } else {
-            const command = message.client.commands.get(args.join(" ").toLowerCase()) || message.client.commands.find(x => x.aliases && x.aliases.includes(args.join(" ").toLowerCase()));
+            .setTitle("Help Commands!")
+            .setDescription("**Type Prefix `e!` + `<command>` to use Commands**" + "\n" + "**Type `e!` + `developer` to see bot creator info 👋**")
+            .setColor("#8de815")
+            .addField("🐶 Animal: ","`cat` `dog`")
+            .addField("🔐 Moderator:","`sv-info` `user-info` `clear` `ban` `kick` `tempmute` `poll` ")
+            .addField("🧾 Feature:","`chat` `avatar` `search` `cfs` `weather` `ping` `math` `dms` `say` `emoji` `emoji-list` `feedback`")
+            .addField("🎵 Music:","`play` `resume` `pause` `loop` `nowplaying` `queue` `clear-quece` `filter` `w-filters` `search` `shuffle` `skip` `stop` `volume`")
+            .addField("🎆 Fun:","`8ball` `iq` `meme` `slap` `kiss` `hug` `baka` `gay`")
+            .addField("⛔ NSFW:","`ass` `anal` `blowjob` `boobs` `cum` `ero` `erofeet` `foxgirl`")
+            .setTimestamp()
+        message.channel.send(embed)
 
-            if (!command) return message.channel.send(`${client.emotes.error} - I did not find this command !`);
-
-            message.channel.send({
-                embed: {
-                    color: 'ORANGE',
-                    author: { name: 'Help pannel' },
-                    footer: { text: 'This bot uses a Github project made by Zerio (ZerioDev/Music-bot)' },
-                    fields: [
-                        { name: 'Name', value: command.name, inline: true },
-                        { name: 'Category', value: command.category, inline: true },
-                        { name: 'Aliase(s)', value: command.aliases.length < 1 ? 'None' : command.aliases.join(', '), inline: true },
-                        { name: 'Utilisation', value: command.utilisation.replace('{prefix}', client.config.discord.prefix), inline: true },
-                    ],
-                    timestamp: new Date(),
-                    description: 'Find information on the command provided.\nMandatory arguments `[]`, optional arguments `<>`.',
-                }
-            });
-        };
-    },
-};
+    }
+}
