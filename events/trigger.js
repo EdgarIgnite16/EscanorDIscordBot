3ad6d663611bff553fc7b6fceea387e83ec143d4
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-07-08 13:07:25 +0700

    12

--- events/trigger.js
+++ events/trigger.js
@@ -1,13 +1,25 @@
 module.exports = { 
     name:"message",
     execute(message){
-        if(message.content.includes("edgar") || message.content.includes("Edgar")){
+        if(message.content.toLowerCase().includes("edgar")){
             message.channel.send("Vua ăn Hại , Vua lì đòn , Vua Xàm Lờ , Vua của các loài lươn !").then((sent) => {
                 setTimeout(() => {
                     sent.delete();
                 }, 10000);
             });
         }
-        
-    }
+        if (message.content.includes("<@!709392910008713288")){
+            message.reply("Đừng ping Ed khùm nữa , nó quạu nó nhốt vô tù bây h :>")
+        }
+
+        if (message.content.startsWith("<@!806542996166017135")){
+            message.channel.send({
+                embed: {
+                    title:"Error 404",
+                    color: 5767167,
+                    description: "Nhập `e!help` để đọc hướng dẫn sử dụng !\nMọi chi tiết xin liên hệ ed khùm để biết thêm chi tiết\n \nxin trân thành cảm ơn !"
+                }
+            })
+        }
+    }   
 }
